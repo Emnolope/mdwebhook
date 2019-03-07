d0b906050a6e1eb98a1d5496ca0008b142bd6795
Emnolope <44859532+Emnolope@users.noreply.github.com>
2019-03-07 12:12:51 -0800

    superficial
    shit

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def process_user(account):
             # Ignore deleted files, folders, and non-markdown files
             if (isinstance(entry, DeletedMetadata) or
                 isinstance(entry, FolderMetadata) or
-                not entry.path_lower.endswith('.pdf')):
+                not entry.path_lower.endswith('.md')):
                 continue
 
             #Do a request
